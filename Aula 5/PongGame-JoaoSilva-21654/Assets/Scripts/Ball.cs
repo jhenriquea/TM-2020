803b5fb3f110c4996b17984ceb291c8198b312e6
jhenriquea <61152747+jhenriquea@users.noreply.github.com>
2020-03-27 13:56:56 +0000

    JoaoSilva-21654

---
+++ Aula 5/PongGame-JoaoSilva-21654/Assets/Scripts/Ball.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+
+    public float speed;
+    public Rigidbody2D rb;
+
+    private Vector2 startPosition;
+    
+    void Start()
+    {
+        launch();
+    }
+
+    private void launch()
+    {
+        float x = Random.Range(0, 2) == 0 ? -1 : 1; //Direção aleatoria eixo x e y
+        float y = Random.Range(0, 2) == 0 ? -1 : 1; //Direção aleatoria eixo x e y
+        rb.velocity = new Vector2(speed * x, speed * y);
+    }
+
+    public void Reset()
+    {
+        rb.velocity = Vector2.zero;
+        transform.position = startPosition;
+        launch();
+    }
+    public void increaseSpeed()
+    {
+        float velx = rb.velocity[0] + (rb.velocity[0] > 0 ? 1 : -1);   //Mantem direcao mas aumenta velocidade
+        float vely = rb.velocity[1] + (rb.velocity[1] > 0 ? 1 : -1);   //Mantem direcao mas aumenta velocidade
+
+        rb.velocity = new Vector2(velx, vely); //Update velocidade com base na velocidade inicial
+    }
+}
---
+++ Aula 5/PongGame-JoaoSilva-21654/Assets/Scripts/Paddle.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour
+{
+
+    public float speed;
+    public Rigidbody2D rb;
+
+    private Vector3 startPosition;
+
+    private float movement;
+
+    void Start()
+    {
+        startPosition = transform.position;   
+    }
+
+    void Update()
+    {
+        movement = Input.GetAxisRaw("Vertical");    //W e S são default ... Retorna como valores -1 ... 1
+        rb.velocity = new Vector2(rb.velocity.x, movement * speed); //Velocidade do paddle
+    }
+
+    public void Reset()
+    {
+        rb.velocity = Vector2.zero; //Reseta velocidade
+        transform.position = startPosition; //Reseta posição
+    }
+}
